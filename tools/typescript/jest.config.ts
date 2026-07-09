65dd96dbea106b12262b604664737cdbee827d0e
Richard Song <48084418+rich-song@users.noreply.github.com>
2026-07-08 22:24:50 -0700

    fix: add jest setup file that polyfills globalThis.crypto with webcrypto

--- tools/typescript/jest.config.ts
+++ tools/typescript/jest.config.ts
@@ -5,10 +5,12 @@ const config: Config = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/test/**/*.ts?(x)'],
+  testPathIgnorePatterns: ['<rootDir>/src/test/setup.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  setupFiles: ['<rootDir>/src/test/setup.ts'],
 };
 
 export default config;
---
+++ tools/typescript/src/test/setup.ts
@@ -0,0 +1,10 @@
+import {webcrypto} from 'node:crypto';
+
+if (!globalThis.crypto) {
+  Object.defineProperty(globalThis, 'crypto', {
+    value: webcrypto,
+    configurable: true,
+    enumerable: false,
+    writable: false,
+  });
+}
